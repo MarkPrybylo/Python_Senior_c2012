75102083a8c62ece4ededd73f45047e25f316576
c2012 <c2012@step.lv>
2023-10-21 13:50:57 +0300

    Update urok1.py

--- urok1.py
+++ urok1.py
@@ -6,8 +6,23 @@
 #     b += a
 #     i += 1
 # print(f"I added {a} {i} times")
-a = ["pineapple", "pear", "cake", "apple", "lemon", "chocolate", "peach", "candy"]
-for i in a:
-    if i == "chocolate" or i == "candy" or i == "cake":
-        a.remove(i)
-print(a)
+# class Student:
+#     def __init__(self, name, age=12):
+#         self.name = name
+#         self.age = age
+#         print(f"Name: {self.name}, age: {self.age}")
+#
+# zlata = Student(name="Zlata", age=13)
+# marko = Student(name="Marko")
+class Animal:
+    def __init__(self, type, weight):
+        self.type = type
+        self.weight = weight
+    def call(self):
+        print(f"Type: {self.type}, weight: {self.weight} kg")
+dog = Animal(type="dog", weight=50)
+dog.call()
+cat = Animal(type="cat", weight=25)
+cat.call()
+parrot = Animal("parrot", 10)
+parrot.call()
