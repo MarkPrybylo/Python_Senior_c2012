a6aba019ba6f721bea7d09b0b039d57c25a6ff47
c2012 <c2012@step.lv>
2023-10-21 14:23:06 +0300

    Update urok1.py

--- urok1.py
+++ urok1.py
@@ -14,20 +14,35 @@
 #
 # zlata = Student(name="Zlata", age=13)
 # marko = Student(name="Marko")
-class Animal:
-    def __init__(self, type, weight):
-        self.type = type
-        self.weight = weight
-    def call(self):
-        print(f"Type: {self.type}, weight: {self.weight} kg")
-dog = Animal(type="dog", weight=50)
-dog.call()
+# class Animal:
+#     def __init__(self, type, weight):
+#         self.type = type
+#         self.weight = weight
+#     def call(self):
+#         print(f"Type: {self.type}, weight: {self.weight} kg")
+# dog = Animal(type="dog", weight=50)
+# dog.call()
+#
+# cat = Animal(type="cat", weight=25)
+# cat.call()
+#
+# parrot = Animal("parrot", 10)
+# parrot.call()
+#
+# capybara = Animal(type="capybara", weight=40)
+# capybara.call()
+made_movies = 0
+class Movie:
+    def __init__(self, name, views, length, madein):
+        global made_movies
+        made_movies += 1
+        self.name = name
+        self.views = views
+        self.length = length
+        self.producer_country = madein
+        print(
+            f"New movie! Name: {self.name}, views: {self.views}, length: {self.length} hours, made in {self.producer_country}")
 
-cat = Animal(type="cat", weight=25)
-cat.call()
-
-parrot = Animal("parrot", 10)
-parrot.call()
-
-capybara = Animal(type="capybara", weight=40)
-capybara.call()
+breaking_bad = Movie("Breaking Bad", 0, 2.5, "USA")
+squid_game = Movie("Squid Game", 0, 3, "South Korea")
+print(f"Made movies in total: {made_movies}")
