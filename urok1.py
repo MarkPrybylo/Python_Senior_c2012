311a508014b445a68542d3b2eefe5a744acf7c8f
c2012 <c2012@step.lv>
2023-10-21 12:56:33 +0300

    Update urok1.py

--- urok1.py
+++ urok1.py
@@ -1,9 +1,13 @@
-a = int(input("Input num: "))
-b = 0
-i = 0
-while b <= 100:
-  print(b)
-  b += a
-  i+= 1
-
-print(f"I added {a} {i} times")
+# a = int(input("Input num: "))
+# b = 0
+# i = 0
+# while b <= 100:
+#     print(b)
+#     b += a
+#     i += 1
+# print(f"I added {a} {i} times")
+a = ["pineapple", "pear", "apple", "peach", "chocolate", "peach", "candy"]
+for i in a:
+    if i == "chocolate" or i == "candy":
+        a.remove(i)
+print(a)
