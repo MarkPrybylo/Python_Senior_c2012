a810af268668a020a2310c88a2fd931d6493d4de
MarkGameDev221 <110909028+MarkGameDev221@users.noreply.github.com>
2023-10-27 22:04:44 +0300

    ДЗ виконане
    Ура ура ура

--- dz2_c2012.py
+++ dz2_c2012.py
@@ -1,1 +1,22 @@
+class Employee:
+    def __init__(self, name, job, company):
+        self.name = name
+        self.job = job
+        self.company = company
+        print(f"Employee {self.name}, work as {self.job} at {self.company}")
+    def change_name(self, new_name):
+        self.name = new_name
+        print(f"New name: {self.name}")
+    def change_job(self, new_job):
+        self.job = new_job
+        print(f"New job: {self.job}")
+    def change_company(self, new_company):
+        self.company = new_company
+        print(f"New company: {self.company}")
+john = Employee("John", "Senior Game Developer", "Nintendo")
+john.change_name("Ben")
+john.change_job("UI Artist")
+john.change_company("Sony")
 
+natalia = Employee("Natalia", "Game Designer", "Valve")
+natalia.change_company("EA")
