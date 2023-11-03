836044e5370fa6c3616e1c91113940fb4b236bc8
MarkGameDev221 <110909028+MarkGameDev221@users.noreply.github.com>
2023-11-03 23:48:32 +0200

    Update urok3.py

--- urok3.py
+++ urok3.py
@@ -5,20 +5,28 @@ a = 0
 b = 10
 c = 0
 a = nominal1 * b
-while a > sum:
-    b -= 1
-    a = nominal1 * b
-    print(f"{a} = {nominal1} * {b}")
-print(f"Першим номіналом вийшло {a} монет ({nominal1} * {b}).")
+def check_first_n():
+    global a
+    global b
+    while a > sum:
+        b -= 1
+        a = nominal1 * b
+        print(f"{a} = {nominal1} * {b}")
+    print(f"Першим номіналом вийшло {a} монет ({nominal1} * {b}).")
+check_first_n()
 sum2 = a + nominal2
 if sum2 > sum:
     a = nominal1 * b
     print(f"{a} = {nominal1} * {b}")
     while a < sum:
         a += nominal2
-        print(f"{sum2} > {a} + {nominal2}")
-        c += 1
-        if sum2 == sum:
+        if a > sum:
+            print(f"\n{a} ({a - nominal2} + {nominal2}) > {sum}\n")
+            check_first_n()
+        else:
+            print(f"{a} = {a - nominal2} + {nominal2}")
+            c += 1
+        if a == sum:
             break
     print(f"Алгоритм завершений. Вам потрібно {b} монет по {nominal1} та {c} монет по {nominal2}:\n{sum} = {nominal1 * b} + {nominal2 * c}")
 else:
